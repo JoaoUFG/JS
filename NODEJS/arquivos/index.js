3aed36cba418543db8b87fda8974f06ec4fc7072
JohnUFG <joao.tavares@discente.ufg.br>
2022-10-11 21:55:01 -0300

    aula 02.02

--- NODEJS/arquivos/index.js
+++ NODEJS/arquivos/index.js
@@ -1,6 +1,25 @@
 import chalk from 'chalk';
+import fs from 'fs';
 
-console.log(chalk.bgBlack("OLA MUNDAO"));
+function pegaArquivo(caminho){
+   
+    const utf = "utf-8";
+    //let textoRetorno = "";
+     return fs.readFileSync(caminho,utf,(_, texto)=>{
+        
+         //console.log(chalk.green(texto));
+         //textoRetorno = texto;
+    });
+
+  
+}
+
+let x = pegaArquivo('./materiais/texto.md').split("(https://");
+
+console.log(x.length);
+
+
+
 
 
 
